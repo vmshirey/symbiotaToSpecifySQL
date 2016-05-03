2afd2ac4dc7a57adb2db03ebe4a3bbfb8c2cea87
Vaughn Shirey <vms44@drexel.edu>
2016-05-03 12:35:20 -0400

    - handle localities

--- sts_1.1.sql
+++ sts_1.1.sql
@@ -1,9 +1,11 @@
+DELETE FROM tempLocality;
 INSERT INTO tempLocality(OccID, Latitude1, Longitude1, MaxElevation, MinElevation, VerbatimElevation, Long1Text, VerbatimLatitude, VerbatimLongitude)
 	SELECT occid, decimalLatitude, decimalLongitude, maximumElevationInMeters, minimumElevationInMeters, verbatimElevation, locality, SUBSTRING_INDEX(vCoord, ' ', 1) AS VerbatimLatitude, 
 	SUBSTRING_INDEX(vCoord, ' ', -1) AS VerbatimLongitude 
 	FROM (SELECT occid, decimalLatitude, decimalLongitude, maximumElevationInMeters, minimumElevationInMeters, verbatimElevation, locality, verbatimCoordinates AS vCoord FROM dwc_view) AS localityTable ORDER BY locality;
 	
 -- BEGIN INSERT WITH TEMPORARY COLLECTION EVENTS --
+DELETE FROM tempColEvent;
 INSERT INTO tempColEvent(OccID, StartDate, VerbatimDate)
 	SELECT occid, eventDate, verbatimEventDate
 	FROM dwc_view;
@@ -14,11 +16,13 @@ INSERT INTO tempColEvent(OccID, StartDate, VerbatimDate)
 -- 	FROM dwc_view AS dwc, tempAgent AS tAgent WHERE dwc.occid = tAgent.occid;
 	
 -- BEGIN INSERT INTO TEMPORARY COLLECTION OBJECT --
+DELETE FROM tempColObject;
 INSERT INTO tempColObject(OccID, AltCatalogNumber, CatalogNumber)
 	SELECT occid, otherCatalogNumbers, catalogNumber
 	FROM dwc_view;
 
 -- BEGIN INSERT INTO TEMPORARY DETERMINATIONS --
+DELETE FROM tempDetermination;
 INSERT INTO tempDetermination(OccID, TaxonID, CollectionObjectID)
 	SELECT dwc.occid, dwc.taxonID, tempColObj.CollectionObjectID 
 	FROM dwc_view AS dwc, tempColObject AS tempColObj WHERE dwc.occid = tempColObj.occid;
--- sts_1.2.sql
+++ sts_1.2.sql
@@ -4,6 +4,9 @@ ADD FOREIGN KEY (AgentID) REFERENCES tempAgent.AgentID;
 ALTER TABLE tempDetermination
 ADD FOREIGN KEY (AgentID) REFERENCES tempAgent.AgentID;
 
+ALTER TABLE tempDetermination
+ADD FOREIGN KEY (CollectionObjectID) REFERENCES tempColObject.TempColObjectID;
+
 INSERT INTO tempCollector(OccID, AgentID)
 SELECT OccID, AgentID FROM tempAgent WHERE tempAgent.AgentType IS NULL;
 
@@ -11,5 +14,19 @@ SELECT OccID, AgentID FROM tempAgent WHERE tempAgent.AgentType IS NULL;
 UPDATE tempCollector JOIN (SELECT OccID, MIN(TempCollectorID) as minValue FROM tempCollector GROUP BY OccID) tMin ON tempCollector.OccID = tMin.OccID AND tempCollector.TempCollectorID = tMin.minValue
 SET isPrimary = 1;
 
+UPDATE tempCollector
+SET CollectorID = AgentID;
+
 UPDATE tempDetermination
 SET AgentID = (SELECT AgentID FROM tempAgent WHERE tempDetermination.OccID = tempAgent.OccID AND tempAgent.AgentType = 2);
+
+UPDATE tempDetermination
+SET CollectionObjectID = (SELECT TempColObjectID FROM tempColObject WHERE tempDetermination.OccID = tempColObject.OccID);
+
+-- handle localities --
+
+UPDATE tempLocality JOIN (SELECT Long1Text, Latitude1, Longitude1, MIN(TempLocalityID) as minValue FROM tempLocality GROUP BY Long1Text) tMin ON tempLocality.Long1Text = tMin.Long1Text OR tempLocality.Latitude1 = tMin.Latitude1 AND tempLocality.Longitude1 = tMin.Longitude1 
+SET LocalityID = tMin.minValue;
+
+UPDATE tempLocality
+SET LocalityID = TempLocalityID WHERE LocalityID IS NULL;
