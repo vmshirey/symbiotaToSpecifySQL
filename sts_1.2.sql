2547867ad413291332bcf4966688db0024fd8f64
Vaughn Shirey <vms44@drexel.edu>
2016-05-03 11:17:32 -0400

    - fixed assignment of primary collectors in tempCollector table

--- sts_1.2.sql
+++ sts_1.2.sql
@@ -4,8 +4,5 @@ ADD FOREIGN KEY (AgentID) REFERENCES tempAgent.AgentID;
 INSERT INTO tempCollector(OccID, AgentID)
 SELECT OccID, AgentID FROM tempAgent;
 
-UPDATE tempCollector
-SET IsPrimary = CASE 
-WHEN AgentID = (SELECT MIN(AgentID)FROM tempAgent AS t WHERE t.OccID = tempCollector.OccID)  THEN '1'
-ELSE NULL
-END
+UPDATE tempCollector JOIN (SELECT OccID, MIN(TempCollectorID) as minValue FROM tempCollector GROUP BY OccID) tMin ON tempCollector.OccID = tMin.OccID AND tempCollector.TempCollectorID = tMin.minValue
+SET isPrimary = 1;
