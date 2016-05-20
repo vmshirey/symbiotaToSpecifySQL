a9c510d4f7750bcebae07d9f208e330ce4ec934a
Vaughn Shirey <vms44@drexel.edu>
2016-05-20 14:45:34 -0400

    - change rankid

--- taxon_reclamation.sql
+++ taxon_reclamation.sql
@@ -24,7 +24,7 @@ DECLARE My_TaxonName varchar(170);
 DECLARE My_RankID varchar(170);
 DECLARE My_ParentID varchar(170);
 DECLARE My_TaxonID varchar(170);
-DECLARE cur CURSOR FOR SELECT SciName, rankID, MAX(parenttid) as parentID, taxaenumtree.tid FROM taxaenumtree, taxa WHERE taxa.tid = taxaenumtree.tid AND rankID = 30 GROUP BY taxaenumtree.tid; 
+DECLARE cur CURSOR FOR SELECT SciName, rankID, MAX(parenttid) as parentID, taxaenumtree.tid FROM taxaenumtree, taxa WHERE taxa.tid = taxaenumtree.tid AND rankID = 10 GROUP BY taxaenumtree.tid; 
 DECLARE CONTINUE HANDLER FOR NOT FOUND SET done := TRUE;
 
 -- SELECT child.SciName, RankID, parent.parentID, child.tid FROM taxa child LEFT JOIN (SELECT MAX(parenttid) as parentID, SciName, taxaenumtree.tid FROM taxaenumtree, taxa WHERE taxa.tid = taxaenumtree.tid GROUP BY taxaenumtree.tid) AS parent ON parent.tid = child.tid WHERE rankID = 10;
