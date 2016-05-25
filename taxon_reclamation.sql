46f30a6c9c3eff44229b5876a579d1f8a4f898e7
Vaughn Shirey <vms44@drexel.edu>
2016-05-25 09:58:45 -0400

    - update

--- taxon_reclamation.sql
+++ taxon_reclamation.sql
@@ -24,7 +24,7 @@ DECLARE My_TaxonName varchar(170);
 DECLARE My_RankID varchar(170);
 DECLARE My_ParentID varchar(170);
 DECLARE My_TaxonID varchar(170);
-DECLARE cur CURSOR FOR SELECT SciName, rankID, MAX(parenttid) as parentID, taxaenumtree.tid FROM taxaenumtree, taxa WHERE taxa.tid = taxaenumtree.tid AND rankID = 10 GROUP BY taxaenumtree.tid; 
+DECLARE cur CURSOR FOR SELECT DISTINCT SciName, rankID, MAX(parenttid) as parentID, taxaenumtree.tid FROM taxaenumtree, taxa WHERE taxa.tid = taxaenumtree.tid GROUP BY taxaenumtree.tid; 
 DECLARE CONTINUE HANDLER FOR NOT FOUND SET done := TRUE;
 
 -- SELECT child.SciName, RankID, parent.parentID, child.tid FROM taxa child LEFT JOIN (SELECT MAX(parenttid) as parentID, SciName, taxaenumtree.tid FROM taxaenumtree, taxa WHERE taxa.tid = taxaenumtree.tid GROUP BY taxaenumtree.tid) AS parent ON parent.tid = child.tid WHERE rankID = 10;
@@ -57,7 +57,7 @@ SET ParentIDInput = ParentIDIn;
 SET ParentName = (SELECT taxa.SciName FROM taxa, taxaenumtree WHERE taxa.tid = taxaenumtree.parenttid AND taxaenumtree.parenttid = ParentIDIn AND taxaenumtree.tid = TaxonIDIn);
 
 INSERT INTO taxon_reclamation(TaxonID, FullName, `Name`, RankID, ParentID, TaxonTreeDefID, TaxonTreeDefItemID, ParentName)
-VALUES (TaxonIDInput, TaxonNameInput, TaxonNameInput, RankIDInput, ParentIDInput, 8, 1, ParentName);
+VALUES (TaxonIDInput, TaxonNameInput, TaxonNameInput, RankIDInput, ParentIDInput, 1, 1, ParentName);
 END //
 
 DELIMITER ;
