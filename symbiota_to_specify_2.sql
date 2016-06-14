7da6f3f428ca94103eebc9e4168ce0f6c9f0386e
Vaughn Shirey <vms44@drexel.edu>
2016-06-14 13:50:31 -0400

    - added code
    fixed rankID assignment
    taxon ParentID reclamation

--- symbiota_to_specify_2.sql
+++ symbiota_to_specify_2.sql
@@ -28,3 +28,33 @@ SELECT now(), 0 as Version, 4 as CollectionMemberID, CollectionEventID + 2017, 4
 -- Change collection code to be appropriate --
 INSERT INTO taxon (TimestampCreated, Version, FullName, Name, RankID, TaxonTreeDefID, TaxonTreeDefItemID, PreviousParentID, ParentName, PreviousTaxonID, CollectionCode)
 SELECT now(), 0 as Version, FullName, Name, RankID, TaxonTreeDefID, 1, ParentID, ParentName, TaxonID, "PH" as CollectionCode FROM taxon_reclamation;
+
+UPDATE taxon
+SET TaxonTreeDefItemID = 1 WHERE RankID = 0;
+UPDATE taxon
+SET TaxonTreeDefItemID = 2 WHERE RankID = 10;
+UPDATE taxon
+SET TaxonTreeDefItemID = 3 WHERE RankID = 30;
+UPDATE taxon
+SET TaxonTreeDefItemID = 9 WHERE RankID = 40;
+UPDATE taxon
+SET TaxonTreeDefItemID = 4 WHERE RankID = 60;
+UPDATE taxon
+SET TaxonTreeDefItemID = 5 WHERE RankID = 100;
+UPDATE taxon
+SET TaxonTreeDefItemID = 6 WHERE RankID = 140;
+UPDATE taxon
+SET TaxonTreeDefItemID = 7 WHERE RankID = 180;
+UPDATE taxon
+SET TaxonTreeDefItemID = 10 WHERE RankID = 190;
+UPDATE taxon
+SET TaxonTreeDefItemID = 8 WHERE RankID = 220;
+UPDATE taxon
+SET TaxonTreeDefItemID = 11 WHERE RankID = 230;
+UPDATE taxon
+SET TaxonTreeDefItemID = 12 WHERE RankID = 240;
+UPDATE taxon
+SET TaxonTreeDefItemID = 13 WHERE RankID = 260;
+
+UPDATE taxon LEFT JOIN (SELECT TaxonID, PreviousTaxonID FROM taxon WHERE CollectionCode = "PH") AS parents ON parents.PreviousTaxonID = taxon.PreviousParentID
+SET taxon.ParentID = parents.TaxonID WHERE CollectionCode = "PH";
