f1f27abdf594bd1bcf0bb5747030987b6a64b451
Vincent <vincent@vincentoleary.com>
2016-09-01 16:35:49 -0400

    Removed Specify dependency for TaxonID

--- Import taxonomy from authority file/003_update_taxonID.sql
+++ Import taxonomy from authority file/003_update_taxonID.sql
@@ -1,8 +1,11 @@
 -- Update taxonID after importing DwC archive to corresponse with new authority file --
 
+UPDATE temptaxonomy SET temptaxonomy.PID = "2" WHERE FullName = "Vertebrata";
+UPDATE temptaxonomy SET temptaxonomy.ParentName = "Animalia" WHERE FullName = "Vertebrata";
+
 -- Run this step if TaxonID already exists --
--- ALTER TABLE dwc_archive
--- DROP COLUMN TaxonID;
+ALTER TABLE dwc_archive
+DROP COLUMN TaxonID;
 
 ALTER TABLE dwc_archive
 ADD COLUMN TaxonID VARCHAR(20);
@@ -10,20 +13,20 @@ ADD COLUMN TaxonID VARCHAR(20);
 -- First set all records to Vertebrate update to search for ID
 
 UPDATE dwc_archive
-SET dwc_archive.TaxonID = 1833967;
+SET dwc_archive.TaxonID = "";
 
 -- Then update for more specific names
 
-UPDATE dwc_archive INNER JOIN (SELECT Name, TaxonID FROM taxon WHERE CollectionCode = "VP") as taxa ON dwc_archive.order = taxa.Name
-SET dwc_archive.TaxonID = taxa.TaxonID;
+UPDATE dwc_archive INNER JOIN (SELECT FullName, TID FROM temptaxonomy) as taxa ON dwc_archive.order = taxa.FullName
+SET dwc_archive.TaxonID = taxa.TID;
 
-UPDATE dwc_archive INNER JOIN (SELECT Name, TaxonID FROM taxon WHERE CollectionCode = "VP") as taxa ON dwc_archive.family = taxa.Name
-SET dwc_archive.TaxonID = taxa.TaxonID;
+UPDATE dwc_archive INNER JOIN (SELECT FullName, TID FROM temptaxonomy) as taxa ON dwc_archive.family = taxa.FullName
+SET dwc_archive.TaxonID = taxa.TID;
 
-UPDATE dwc_archive INNER JOIN (SELECT Name, TaxonID FROM taxon WHERE CollectionCode = "VP") as taxa ON dwc_archive.genus = taxa.Name
-SET dwc_archive.TaxonID = taxa.TaxonID;
+UPDATE dwc_archive INNER JOIN (SELECT FullName, TID FROM temptaxonomy) as taxa ON dwc_archive.genus = taxa.FullName
+SET dwc_archive.TaxonID = taxa.TID;
 
-UPDATE dwc_archive INNER JOIN (SELECT Name, TaxonID FROM taxon WHERE CollectionCode = "VP") as taxa ON dwc_archive.specificEpithet = taxa.Name
-SET dwc_archive.TaxonID = taxa.TaxonID;
+UPDATE dwc_archive INNER JOIN (SELECT FullName, TID FROM temptaxonomy) as taxa ON dwc_archive.specificEpithet = taxa.FullName
+SET dwc_archive.TaxonID = taxa.TID;
 
 -- Some valid names may not match because they were not in the authority file to begin with
