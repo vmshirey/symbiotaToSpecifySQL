60602577272915e5b273abb155a75df3212b4b2c
Vaughn Shirey <vms44@drexel.edu>
2016-09-08 13:48:31 -0400

    added comments to clarify
    Some collectionCode references need to be changed depending on the
    collection being uploaded. This mostly impacts reconciling the previous
    taxonomy ID's with those in Specify.

--- 003_specify_integrate.sql
+++ 003_specify_integrate.sql
@@ -45,18 +45,18 @@ INSERT INTO collectionobject (TimestampCreated, Version, CollectionMemberID, Col
 SELECT now(), 0 as Version, 4 as CollectionMemberID, CollectingEventID + previousColEventMax, 4 as CollectionID, CatalogNumber, AltCatalogNumber, occid 
 FROM tempColObject, specifyIDReference WHERE specifyIDReference.placeholderKey = 1;
 
--- Insert taxonomy --
+-- Insert taxonomy (USE APPROPRIATE COLLECTION CODE)--
 INSERT INTO taxon (TimestampCreated, IsAccepted, IsHybrid, Version, FullName, `Name`, RankID, TaxonTreeDefID, TaxonTreeDefItemID, PreviousParentID, ParentName, PreviousTaxonID, CollectionCode)
 SELECT now(), 1 as IsAccepted, 0 as IsHybrid, Version, FullName, SUBSTRING_INDEX(`FullName`, ' ', -1) as `Name`, RankID, TaxonTreeDefID, 1 as TaxonTreeDefItemID, PreviousPID, ParentName, PreviousTID, 'VP' as CollectionCode FROM taxon_reclamation;
 
 UPDATE taxon (SELECT taxon.RankID, rankIDDef.TaxonTreeDefItemID FROM taxon INNER JOIN rankIDDef) AS taxrank ON taxrank.RankID = rankIDDef.rankID
-SET taxon.TaxonTreeDefItemID = taxrank.TaxonTreeDefItemID WHERE CollectionCode = '';
+SET taxon.TaxonTreeDefItemID = taxrank.TaxonTreeDefItemID WHERE CollectionCode = ''; -- Collection code needs to be changed depending on import
 
 UPDATE taxon INNER JOIN (SELECT TaxonID, PreviousTaxonID FROM taxon WHERE CollectionCode = '') AS parents ON parents.PreviousTaxonID = taxon.PreviousParentID
-SET taxon.ParentID = parents.TaxonID WHERE CollectionCode = '';
+SET taxon.ParentID = parents.TaxonID WHERE CollectionCode = ''; -- Collection code needs to be changed depending on import
 
 UPDATE taxon INNER JOIN (SELECT TaxonID, PreviousTaxonID, Name FROM taxon WHERE CollectionCode = '') AS parents ON parents.PreviousTaxonID = taxon.PreviousParentID
-SET taxon.ParentName = parents.Name WHERE CollectionCode = '';
+SET taxon.ParentName = parents.Name WHERE CollectionCode = ''; -- Collection code needs to be changed depending on import
 
 -- Insert determinations for reassociation with Specify taxonomy --
 INSERT INTO determination (TimestampCreated, Version, CollectionMemberID, oldTaxonID, CollectionObjectID, DeterminerID)
@@ -64,7 +64,7 @@ SELECT collectionobject.TimestampCreated, 1 as Version, 4 as CollectionMemberID,
 FROM tempDetermination, collectionobject;
 
 -- Update determinations to corresponse with new taxonomy tree --
-UPDATE determination INNER JOIN (SELECT TaxonID FROM taxon WHERE CollectionCode = "") as taxa ON determination.oldTaxonID = taxa.TaxonID
+UPDATE determination INNER JOIN (SELECT TaxonID FROM taxon WHERE CollectionCode = "") as taxa ON determination.oldTaxonID = taxa.TaxonID -- Collection code needs to be changed depending on import
 SET determination.TaxonID = taxa.TaxonID, PreferredTaxonID = taxa.TaxonID, IsCurrent = 1;
 
 -- Update Localities --
